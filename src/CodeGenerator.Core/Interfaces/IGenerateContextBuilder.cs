1e6765aa7183df73f4e01f944b46879c5bce2beb
高飞 <fei.gao8@live.cn>
2021-04-04 15:57:15 +0800

    add IGenerateContextBuilder

---
+++ src/CodeGenerator.Core/Interfaces/IGenerateContextBuilder.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using CodeGenerator.Core.Implements;
+
+namespace CodeGenerator.Core.Interfaces
+{
+    public interface IGenerateContextBuilder<in TDbTable, in TDbFields>
+    {
+        IGenerateContext GenerateContext { get; set; }
+        Task<IGenerateContext> BuildContext();
+    }
+}
