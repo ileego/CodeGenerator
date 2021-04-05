9c53445acaa4c439c5f7019de947371a41f64ad9
高飞 <fei.gao8@live.cn>
2021-04-05 23:18:18 +0800

    删除测试内容

--- src/CodeGenerator.Core/ForTest/Repository/IUserRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using CodeGenerator.Core.ForTest.Entities;
-using CodeGenerator.Infra.Common.Repository;
-
-namespace CodeGenerator.Core.ForTest.Repository
-{
-    public interface IUserRepository : IEfRepository<User, long>
-    {
-    }
-}
