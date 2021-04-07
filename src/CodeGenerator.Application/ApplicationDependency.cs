fd788ffae0bdf93f18eec74133a172dd480f00e9
高飞 <fei.gao8@live.cn>
2021-04-07 17:07:48 +0800

    增加依赖注入

---
+++ src/CodeGenerator.Application/ApplicationDependency.cs
@@ -0,0 +1,20 @@
+﻿using CodeGenerator.Infra.Common.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CodeGenerator.Application
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class ApplicationDependency
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="services"></param>
+        public static void AddApplicationDependency(this IServiceCollection services)
+        {
+            services.AddScopeAssembly(typeof(ApplicationDependency).Assembly, "", "Service");
+        }
+    }
+}
