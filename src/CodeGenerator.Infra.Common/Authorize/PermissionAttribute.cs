ef14ab899de78e62f79acab63f86bc961e7a0ef0
高飞 <fei.gao8@live.cn>
2021-05-17 16:30:36 +0800

    修改PermissionAttribute，修改Controller生成模板

--- src/CodeGenerator.Infra.Common/Authorize/PermissionAttribute.cs
+++ src/CodeGenerator.Infra.Common/Authorize/PermissionAttribute.cs
@@ -14,18 +14,28 @@ namespace CodeGenerator.Infra.Common.Authorize
     public class PermissionAttribute : AuthorizeAttribute
     {
         /// <summary>
+        /// 分组标识
+        /// </summary>
+        public string GroupTag { get; private set; }
+        /// <summary>
         /// 分组名称
         /// </summary>
         public string GroupName { get; private set; }
+        /// <summary>
+        /// Action标识
+        /// </summary>
+        public string ActionTag { get; private set; }
         /// <summary>
         /// Action名称
         /// </summary>
         public string ActionName { get; private set; }
 
-        public PermissionAttribute(string groupName, string actionName)
+        public PermissionAttribute(string groupTag, string groupName, string actionTag, string actionName)
             : base(Permission.Policy)
         {
+            this.GroupTag = groupTag;
             this.GroupName = groupName;
+            this.ActionTag = actionTag;
             this.ActionName = actionName;
         }
     }
